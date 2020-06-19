239d1c624ebcc1d388d108682d06012cc5847e3b
d-tsuji <dram.dt.shonan@gmail.com>
2020-06-19 20:37:59 +0900

    use environment variable for table name

--- db.go
+++ db.go
@@ -1,6 +1,7 @@
 package sample_circleci
 
 import (
+	"log"
 	"os"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -8,11 +9,20 @@ import (
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 )
 
-var db *dynamodb.DynamoDB
+var (
+	db *dynamodb.DynamoDB
+
+	userTable string
+)
 
 func init() {
 	dbEndpoint := os.Getenv("DYNAMO_ENDPOINT")
 	region := os.Getenv("AWS_REGION")
+
+	userTable = os.Getenv("DYNAMO_TABLE_USER")
+	if userTable == "" {
+		log.Fatal(`env variable "DYNAMO_TABLE_USER" is required`)
+	}
 
 	sess := session.Must(session.NewSession(&aws.Config{
 		Endpoint: aws.String(dbEndpoint),
